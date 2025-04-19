53334d21a89d1fb2896ac1ef85ed89e4f90e1a8d
Geleta Tamiru <geletatamiru@yahoo.com>
2025-04-19 11:43:45 +0300

    created a vited project and added hheader.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Header.css"
+export default function Header() {
+  return (
+    <div className='header'>
+      <div>Logo</div>
+      <div>Store</div>
+      <div>Mac</div>
+      <div>iPad</div>
+      <div>iPhone</div>
+      <div>Watch</div>
+      <div>Vision</div>
+      <div>AirPods</div>
+      <div>Tv & Home</div>
+      <div>Entertainment</div>
+      <div>Accessories</div>
+      <div>Support</div>
+      <div>Search</div>
+      <div>Cart</div>
+    </div>
+  )
+}
